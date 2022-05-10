dd3bd2e99d0a5903f96e89d041a281365df2bc58
JHanna-2020 <66224091+JHanna-2020@users.noreply.github.com>
2022-05-10 12:19:26 -0500

    Add files via upload

---
+++ ONN.py
@@ -0,0 +1,130 @@
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter.font import Font
+import pygame
+import time
+from mutagen.mp3 import MP3
+
+pygame.mixer.init()
+
+def oNimNai():
+    var = 2
+    global topImg
+    global audio
+
+    def audio():
+        x = 1
+        if x <= 1:
+            aLabel = Label(res, text="\nAudio")
+            aLabel.pack();
+            song_list = Listbox(res, bg="#e9e9e9", width=60, borderwidth=0, height=5)
+            song1 = "/Volumes/My Passport/Programming/Python/PyApp/audio/O Nim Nai Ibrahim Ayad.mp3"
+            song1 = song1.replace("/Volumes/My Passport/Programming/Python/PyApp/audio/", "")
+            song1 = song1.replace(".mp3", "")
+            song2 = "/Volumes/My Passport/Programming/Python/PyApp/audio/O Nim Nai St Mark JC Deacons.mp3"
+            song2 = song2.replace("/Volumes/My Passport/Programming/Python/PyApp/audio/", "")
+            song2 = song2.replace(".mp3", "")
+
+            def song_length():
+                current_time = pygame.mixer.music.get_pos() / 1000
+                mins_time = time.strftime('%M:%S', time.gmtime(current_time))
+
+                statusBar.after(1000, song_length)
+
+                current_song = song_list.curselection()
+                song = song_list.get(current_song)
+                song = f'/Volumes/My Passport/Programming/Python/PyApp/audio/{song}.mp3'
+                song_mut = MP3(song)
+                song_time = song_mut.info.length
+                song_total = time.strftime('%M:%S', time.gmtime(song_time))
+
+                statusBar.config(text=f'{mins_time}/{song_total}')
+
+            def play():
+                song = song_list.get(ACTIVE)
+                song = f'/Volumes/My Passport/Programming/Python/PyApp/audio/{song}.mp3'
+                pygame.mixer.music.load(song)
+                pygame.mixer.music.play(loops=0)
+                song_length()
+
+            def stop():
+                pygame.mixer.music.stop()
+                song_list.selection_clear(ACTIVE)
+
+            global paused
+            paused = False
+
+            def pause(is_paused):
+                global paused
+                paused = is_paused
+                if paused:
+                    pygame.mixer.music.unpause()
+                    paused = False
+                else:
+                    pygame.mixer.music.pause()
+                    paused = True
+
+            def next_song():
+                next_one = song_list.curselection()
+                next_one = next_one[0] + 1
+                song = song_list.get(next_one)
+                song = f'/Volumes/My Passport/Programming/Python/PyApp/audio/{song}.mp3'
+                pygame.mixer.music.load(song)
+                pygame.mixer.music.play(loops=0)
+                song_list.selection_clear(0, END)
+                song_list.activate(next_one)
+                song_list.selection_set(next_one, last=None)
+
+            def previous_song():
+                next_one = song_list.curselection()
+                next_one = next_one[0] - 1
+                song = song_list.get(next_one)
+                song = f'/Volumes/My Passport/Programming/Python/PyApp/audio/{song}.mp3'
+                pygame.mixer.music.load(song)
+                pygame.mixer.music.play(loops=0)
+                song_list.selection_clear(0, END)
+                song_list.activate(next_one)
+                song_list.selection_set(next_one, last=None)
+
+            song_list.insert(END, song1)
+            song_list.insert(END, song2)
+            song_list.pack()
+            backBtn = Button(res, text="⏪", borderwidth=0, command=previous_song)
+            playBtn = Button(res, text="▶️", borderwidth=0, command=play)
+            pauseBtn = Button(res, text="⏸️", borderwidth=0, command=lambda: pause(paused))
+            forwardBtn = Button(res, text="⏩️", borderwidth=0, command=next_song)
+            stopBtn = Button(res, text="⏹️", borderwidth=0, command=stop)
+            statusBar = Label(res, text='00:00/00:00', bd=1, relief=GROOVE, anchor=E, bg="white", fg="black")
+            statusBar.pack(ipady=2)
+            backBtn.pack()
+            playBtn.pack()
+            pauseBtn.pack()
+            forwardBtn.pack()
+            stopBtn.pack()
+
+    res = Toplevel()
+
+    res.title("O Nim Nai")
+    res.geometry("1500x1500")
+
+    cHeading = Font(family="CS Avva Shenouda", size=20)
+    englishFont = Font(family="Helvetica", size=15)
+    copticFont = Font(family="CS Avva Shenouda", size=18)
+    khristosanestee = Label(res,
+                            text="`w nim naicumvwnia =g", font=copticFont)
+    english_KA = Label(res,
+                       text="(Oh what are these symphonies)3", font=englishFont)
+
+    kaHeading = Label(res, text="`w nim nai", font=cHeading)
+    topImg = ImageTk.PhotoImage(Image.open("pic.png"))
+    topLab = Label(res, image=topImg)
+    topLab.pack()
+    greek = Label(res, text="Coptic")
+    english = Label(res, text="English")
+    kaHeading.pack()
+    greek.pack()
+    khristosanestee.pack()
+    english.pack()
+
+    english_KA.pack()
+    audio()
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+# packages
 from tkinter import *
 from PIL import ImageTk, Image
 from tkinter.font import Font
+from tkmacosx import Button
 
+# Hymns
 import CIR
 import HN
 import TS
 import PR
-
+import KAG
+import ONN
+import AYH
+import CIRShort
 root = Tk()
 
 
 #   Home Screen
 
 
-copticFont = Font(family="CS Avva Shenouda", size=20)
+copticFont = Font(family="CS Avva Shenouda", size=16)
+englishFont = Font(family="Helvetica", size=15)
 
 season = Label(root, text="Reserrection", fg="blue")
 root.title("Coptic Hymns")
@@ -22,29 +29,59 @@ root.geometry("1000x1000")
 season.grid(row=0, column=0)
 var = 0
 
-ressurection1 = Button(root, text="`<rictoc `anecty", font=copticFont, command=lambda: [CIR.KA()])
-ressurection2 = Button(root, text="Ton cunanar,on ", font=copticFont, command=lambda: [TS.tonSeena()])
-ressurection3 = Button(root, text="Hiten ", font=copticFont, command=lambda: [HN.hiten()])
-ressurection4 = Button(root, text="<ere Tef`anactacic", font=copticFont, command=lambda: [PR.praxisResponse()])
+ressurection1 = Button(root, text="Hiten ", font=copticFont, bg="black", fg="white", command=lambda: [HN.hiten()])
+ressurection2 = Button(root, text="<ere Tef`anactacic", font=copticFont, bg="black", fg="white", command=lambda: [PR.praxisResponse()])
+ressurection3 = Button(root, text="Kata ni,oroc", font=copticFont, bg="black", fg="white", command=lambda: [KAG.kataNiGreat()])
+ressurection4 = Button(root, text="`W nim nai", font=copticFont, bg="black", fg="white", command=lambda: [ONN.oNimNai()])
+ressurection5 = Button(root, text="Ni,oroc tyrou", font=copticFont, bg="black", fg="white", command=lambda: [AYH.AllYouHeavenly()])
+ressurection6 = Button(root, text="`<rictoc `anecty (2)", font=copticFont, bg="black", fg="white", command=lambda: [CIRShort.KA()])
+ressurection7 = Button(root, text="`<rictoc `anecty", font=copticFont, bg="black", fg="white", command=lambda: [CIR.KA()])
+ressurection8 = Button(root, text="Ton cunanar,on ", font=copticFont, bg="black", fg="white", command=lambda: [TS.tonSeena()])
 
-myImg4 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
-img4Label = Label(image=myImg4)
-img4Label.grid(row=1, column=0)
-ressurection4.grid(row=2, column=1)
+lineBreak = Label(text="\n")
 
 
 myImg1 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
 img1Label = Label(image=myImg1)
-img1Label.grid(row=1, column=1)
-ressurection1.grid(row=2, column=2)
+img1Label.grid(row=1, column=0)
+ressurection1.grid(row=2, column=0)
 
 myImg2 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
 img2Label = Label(image=myImg1)
-img2Label.grid(row=1, column=2)
-ressurection2.grid(row=2, column=3)
+img2Label.grid(row=1, column=1)
+ressurection2.grid(row=2, column=1)
 
 myImg3 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
 img3Label = Label(image=myImg1)
-img3Label.grid(row=1, column=3)
-ressurection3.grid(row=2, column=0)
+img3Label.grid(row=1, column=2)
+ressurection3.grid(row=2, column=2)
+
+myImg4 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
+img4Label = Label(image=myImg4)
+img4Label.grid(row=1, column=3)
+ressurection4.grid(row=2, column=3)
+
+myImg5 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
+img5Label = Label(image=myImg1)
+img5Label.grid(row=1, column=4)
+ressurection5.grid(row=2, column=4)
+
+lineBreak.grid(row=3, column=0)
+myImg6 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
+img6Label = Label(image=myImg1)
+img6Label.grid(row=4, column=1)
+ressurection6.grid(row=5, column=1)
+
+myImg7 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
+img7Label = Label(image=myImg1)
+img7Label.grid(row=4, column=0)
+ressurection7.grid(row=5, column=0)
+
+myImg8 = ImageTk.PhotoImage(Image.open("Imgs/resImg.jpeg"))
+img8Label = Label(image=myImg1)
+img8Label.grid(row=4, column=2)
+ressurection8.grid(row=5, column=2)
+
 root.mainloop()
+
+
